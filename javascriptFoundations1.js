1d9a7dad326a2fd546aad83a806a847364071cd5
Luke Taylor - Work <135564128+Higana76@users.noreply.github.com>
2023-09-07 13:27:55 +0900

    Javascript Foundations 1 complete

---
+++ javascriptFoundations1.js
@@ -0,0 +1,62 @@
+console.log("Hello, World!")
+
+// https://javascript.info/variables
+// Task 1
+let admin;
+let name;
+name = "John";
+admin = name;
+console.log(admin);
+
+// Task 2
+let ourPlanet = "Earth";
+let currentUser = "John";
+console.log(ourPlanet);
+
+// https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Math
+// Test
+
+const lotsOfDecimal = 1.766584958675746364;
+lotsOfDecimal;
+const twoDecimalPlaces = lotsOfDecimal.toFixed(2);
+console.log(twoDecimalPlaces);
+
+
+
+// https://www.theodinproject.com/lessons/foundations-fundamentals-part-1
+// Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+console.log (10 + 5)
+
+// Add a sequence of 6 different numbers together.
+console.log (1 + 2 + 3 + 4 + 5 + 6)
+
+// Print the solution to the following equation: (4 + 6 + 9) / 77
+console.log ((4 + 6 + 9) /77)
+
+
+// Let’s use variables!
+// Type this statement at the top of the script tag: let a = 10
+let a = 10
+
+// In the console console.log(a) should print 10
+console.log(a);
+
+// Try the following in the console: 9 * a
+a = 9 * a;
+
+// and this: let b = 7 * a (returns undefined *) and then console.log(b)
+let b = 7 * a;
+console.log(b);
+
+// You should be getting the hang of this by now… try this sequence:
+// Declare a constant variable max with the value 57
+const max = 57;
+
+// Set another variable actual to max - 13
+const actual = max - 13;
+
+// Set another variable percentage to actual / max
+percentage = actual / max;
+
+// If you type percentage in the console and press Enter you should see a value like 0.7719
+console.log(percentage);
